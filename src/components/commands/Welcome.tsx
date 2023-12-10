b791e0aff29658be2d799017679a80596d6d92a7
Idot07 <90293846+Idot07@users.noreply.github.com>
2023-12-10 05:51:35 +0000

    a

--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -13,31 +13,15 @@ const Welcome: React.FC = () => {
   return (
     <HeroContainer data-testid="welcome">
       <div className="info-section">
-        <PreName>
-          {`        
-    _____       __     _   __      _            
-   / ___/____ _/ /_   / | / /___ _(_)___  ____ _
-   \\__ \\/ __ \`/ __/  /  |/ / __ \`/ / __  / __ \`/
-  ___/ / /_/ / /_   / /|  / /_/ / / / / / /_/ / 
- /____/\\__,_/\\___/ /_/ |_/\\__,_/_/_/ /_/\\__, /  
-                                       /____/   
+        {/* <PreName>
+          {`           
           `}
-        </PreName>
+        </PreName> */}
         <PreWrapper>
-          <PreNameMobile>
+          {/* <PreNameMobile>
             {`
-    ____     __          
-   / __/__ _/ /_         
-  _\\ \\/ _ \`/ __/         
- /___/\\_,_/\\__/          
-    _  __     _          
-   / |/ /__ _(_)__  ___ _
-  /    / _ \`/ / _ \\/ _ \`/
- /_/|_/\\_,_/_/_//_/\\_, / 
-                  /___/  
- 
-          `}
-          </PreNameMobile>
+            `}
+          </PreNameMobile> */}
         </PreWrapper>
         <div>Welcome to the terminal. (Version 1.3.1)</div>
         <Seperator>----</Seperator>
